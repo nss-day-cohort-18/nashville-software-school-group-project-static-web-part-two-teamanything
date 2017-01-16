be11a65a2e3a6641759b21266cc030a285b018ea
Talbot <talbotlawrence75@gmail.com>
2017-01-16 14:55:09 -0600

    added javascript

--- js/main.js
+++ js/main.js
@@ -1,37 +1,76 @@
-	var widget1 = {
+var productsList = [
+
+	{
+		name: "yeah",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yaa",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yay",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yeah",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yeah",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yeah",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yeah",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
+		name: "yeah",
+		description: "It is wonderful",
+		price: "$5.00",
+		url: "http:/"
+	},
+
+	{
 		name: "yeah",
 		description: "It is wonderful",
 		price: "$5.00",
 		url: "http:/"
 	}
-
-	var widget2 = {
-		name: "yaa",
-		description: "It is wonderful",
-		price: "$5.00",
-		url: "http:/"
-	}
-
-	var widget3 = {
-		name: "yay",
-		description: "It is wonderful",
-		price: "$5.00",
-		url: "http:/"
-	}
+]	
 //////////////////////////////////////////////////////////////////
-	var widgetList = [
-		widget1,
-		widget2,
-		widget3
-	]
-
-for (var i = 0; i < widgetList.length; i++) {              //start loop
-	var container = document.createElement("div");         //create the <div> tag
-	container.id = "card" + (i + 1);                       //create and name the div id="card1,2,3 etc."
+for (var i = 0; i < productsList.length; i++) {              //start loop
+	var container = document.createElement("article");         //create the <article> tag
+	container.class = "card";                       //create and name the article class="card1,2,3 etc."
 	container.innerHTML =                                  //physically create the list
-		"<li>" + widgetList[i].name + "</li>" +            
-		"<li>" + widgetList[i].price + "</li>" + 
-		"<li>" + widgetList[i].description + "</li>" +
-		"<li>" + widgetList[i].url + "</li>" + "</div>";
+		"<h3>" + productsList[i].name + "</h3>" +            
+		"<p>" + productsList[i].price + "</p>" + 
+		"<p>" + productsList[i].description + "</p>" +
+		"<img src=\"\" class=\"images/image1.jpg\">" + productsList[i].url + ">" + "</article>";
 	document.body.appendChild(container);                  //physically add to HTML
 }
