a06ffdd9e59159014352625fd9def907e9d331c6
Talbot <talbotlawrence75@gmail.com>
2017-01-16 15:36:25 -0600

    finished the for loop

--- js/main.js
+++ js/main.js
@@ -64,13 +64,13 @@ var productsList = [
 	}
 ]	
 //////////////////////////////////////////////////////////////////
-for (var i = 0; i < productsList.length; i++) {              //start loop
+for (var i = 1; i < productsList.length; i++) {              //start loop
 	var container = document.createElement("article");         //create the <article> tag
-	container.class = "card";                       //create and name the article class="card1,2,3 etc."
+	container.setAttribute("class","main-content");                       //create and name the article class="card1,2,3 etc."
 	container.innerHTML =                                  //physically create the list
 		"<h3>" + productsList[i].name + "</h3>" +            
 		"<p>" + productsList[i].price + "</p>" + 
 		"<p>" + productsList[i].description + "</p>" +
-		"<img src=\"images/image1.jpg\" class=\"card-images\">" + productsList[i].url + ">" + "</article>";
+		"<img src=\"img/mushroom-" + i + "-300X300.jpg\" class=\"card-images\">" + "</article>";
 	document.body.appendChild(container);                  //physically add to HTML
 }
