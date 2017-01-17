a31d2047439a25b5ee105014c4baf70558f472a7
Talbot <talbotlawrence75@gmail.com>
2017-01-16 19:20:39 -0600

    wrestling with javascript

--- js/main.js
+++ js/main.js
@@ -64,13 +64,29 @@ var productsList = [
 	}
 ]	
 //////////////////////////////////////////////////////////////////
-for (var i = 1; i < productsList.length; i++) {              //start loop
-	var container = document.createElement("article");         //create the <article> tag
-	container.setAttribute("class","main-content");                       //create and name the article class="card1,2,3 etc."
-	container.innerHTML =                                  //physically create the list
+var mainContent = document.getElementsByClassName("main-content");
+
+for (var i = 1; i < productsList.length; i++) {                            //start loop
+	var mainContent = document.createElement("section");                 //create the <article> tag
+	console.log("mainContent", mainContent);
+	mainContent.setAttribute("class","card");                            //create and name the article class="card1,2,3 etc."
+	var mainTags =                                             //physically create the list
 		"<h3>" + productsList[i].name + "</h3>" +            
 		"<p>" + productsList[i].price + "</p>" + 
 		"<p>" + productsList[i].description + "</p>" +
-		"<img src=\"img/mushroom-" + i + "-300X300.jpg\" class=\"card-images\">" + "</article>";
-	document.body.appendChild(container);                  //physically add to HTML
+		"<img src=\"img/mushroom-" + i + "-300X300.jpg\" class=\"card-images\">";// + "</section>";
+	console.log("mainTags", mainTags);
+	mainContent.appendChild(mainTags);
 }
+// var mainContent = document.getElementsByClassName("main-content")
+
+// for (var i = 1; i < productsList.length; i++) {                            //start loop
+// 	var container = document.createElement("section");                 //create the <article> tag
+// 	container.setAttribute("class","card");                            //create and name the article class="card1,2,3 etc."
+// 	container.innerHTML =                                             //physically create the list
+// 		"<h3>" + productsList[i].name + "</h3>" +            
+// 		"<p>" + productsList[i].price + "</p>" + 
+// 		"<p>" + productsList[i].description + "</p>" +
+// 		"<img src=\"img/mushroom-" + i + "-300X300.jpg\" class=\"card-images\">" //+ "</section>";
+// 	document.mainContent.appendChild(container);                                  //physically add to HTML
+// }
