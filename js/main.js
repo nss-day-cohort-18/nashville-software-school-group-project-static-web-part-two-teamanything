8069aff08274e439b6b350493d36c1784549b487
Talbot <talbotlawrence75@gmail.com>
2017-01-16 19:56:38 -0600

    fixed javascript

--- js/main.js
+++ js/main.js
@@ -67,26 +67,11 @@ var productsList = [
 var mainContent = document.getElementsByClassName("main-content");
 
 for (var i = 1; i < productsList.length; i++) {                            //start loop
-	var mainContent = document.createElement("section");                 //create the <article> tag
-	console.log("mainContent", mainContent);
-	mainContent.setAttribute("class","card");                            //create and name the article class="card1,2,3 etc."
-	var mainTags =                                             //physically create the list
+	var mainSection = document.createElement("section");                 //create the <article> tag
+	mainSection.setAttribute("class","card");                            //create and name the article class="card1,2,3 etc."
+	mainSection.innerHTML =                                              //physically create the list
 		"<h3>" + productsList[i].name + "</h3>" +            
 		"<p>" + productsList[i].price + "</p>" + 
 		"<p>" + productsList[i].description + "</p>" +
 		"<img src=\"img/mushroom-" + i + "-300X300.jpg\" class=\"card-images\">";// + "</section>";
-	console.log("mainTags", mainTags);
-	mainContent.appendChild(mainTags);
-}
-// var mainContent = document.getElementsByClassName("main-content")
-
-// for (var i = 1; i < productsList.length; i++) {                            //start loop
-// 	var container = document.createElement("section");                 //create the <article> tag
-// 	container.setAttribute("class","card");                            //create and name the article class="card1,2,3 etc."
-// 	container.innerHTML =                                             //physically create the list
-// 		"<h3>" + productsList[i].name + "</h3>" +            
-// 		"<p>" + productsList[i].price + "</p>" + 
-// 		"<p>" + productsList[i].description + "</p>" +
-// 		"<img src=\"img/mushroom-" + i + "-300X300.jpg\" class=\"card-images\">" //+ "</section>";
-// 	document.mainContent.appendChild(container);                                  //physically add to HTML
-// }
+	mainContent[0].appendChild(mainSection);
